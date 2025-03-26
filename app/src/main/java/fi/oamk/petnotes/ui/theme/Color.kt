d57e228fe16b19b51e092bc5a6f145f8da0cf2b4
JaneWong2023 <ZhiyingHuang2023@outlook.com>
2025-03-26 17:35:29 +0200

    Add Color variables

--- app/src/main/java/fi/oamk/petnotes/ui/theme/Color.kt
+++ app/src/main/java/fi/oamk/petnotes/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//some color variables for our app
+val PrimaryColor = Color(0xFFEFEFEF)
+val SecondaryColor = Color(0xFF9C9C9C)
+
+val BgColor = Color (0xFFC7C7C7)
+
+val InputColor = Color(0xFFECECEC)
+
+val ButtonColor = Color(0xFFD3D3D3)
