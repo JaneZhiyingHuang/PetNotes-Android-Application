1465cf72eb2dcb9ad5bef0cf869f91470f178323
JaneWong2023 <ZhiyingHuang2023@outlook.com>
2025-03-27 17:17:45 +0200

    AddNewPetScreen: avatar icon added

--- app/src/main/java/fi/oamk/petnotes/ui/AddNewPetScreen.kt
+++ app/src/main/java/fi/oamk/petnotes/ui/AddNewPetScreen.kt
@@ -2,8 +2,11 @@ package fi.oamk.petnotes.ui
 
 import android.renderscript.ScriptGroup.Input
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -12,8 +15,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -38,6 +43,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -47,12 +53,13 @@ import androidx.navigation.NavController
 import fi.oamk.petnotes.viewmodel.AddNewPetViewModel
 import kotlinx.coroutines.launch
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
 import fi.oamk.petnotes.ui.theme.ButtonColor
 import fi.oamk.petnotes.ui.theme.InputColor
-
+import fi.oamk.petnotes.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -98,6 +105,29 @@ fun AddNewPetScreen(navController: NavController) {
                 .padding(16.dp)
                 .verticalScroll(rememberScrollState())
         ) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(107.dp)
+                        .clip(CircleShape)
+                        .background(InputColor) // Placeholder color if no image is uploaded
+                        .clickable {
+
+                        },
+                    contentAlignment = Alignment.Center // Center the icon inside the circle
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.add_a_photo_24px),
+                        contentDescription = "Upload Avatar",
+                        modifier = Modifier.size(40.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
             // Reusable labeled text fields
             LabeledTextField(
                 label = "Pet Name",
