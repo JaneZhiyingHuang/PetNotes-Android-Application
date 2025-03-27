0b58c43f8efbe0e4e5080a8f55dc2cf6d4892ad3
JaneWong2023 <ZhiyingHuang2023@outlook.com>
2025-03-27 20:21:47 +0200

    HomeScreen:card UI added

--- app/src/main/java/fi/oamk/petnotes/ui/HomeScreen.kt
+++ app/src/main/java/fi/oamk/petnotes/ui/HomeScreen.kt
@@ -1,6 +1,9 @@
 package fi.oamk.petnotes.ui
 
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,6 +12,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -29,14 +35,17 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import fi.oamk.petnotes.R
 import fi.oamk.petnotes.model.Pet
+import fi.oamk.petnotes.ui.theme.InputColor
 import fi.oamk.petnotes.viewmodel.HomeScreenViewModel
 import kotlinx.coroutines.launch
 
@@ -90,50 +99,113 @@ fun HomeScreen(
     ) { paddingValues ->
         Column(modifier = Modifier
             .fillMaxSize()
-            .padding(paddingValues)) {
+            .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             if (isUserLoggedIn) {
                 if (pets.isNotEmpty()) {
                     // Display pets in cards
                     pets.forEach { pet ->
                         Card(
-                            shape = RoundedCornerShape(16.dp),
-                            colors = CardDefaults.cardColors(containerColor = Color(0xFFF8F8F8)),
+                            shape = RoundedCornerShape(15.dp),
+                            colors = CardDefaults.cardColors(containerColor = Color.White),
+                            elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
                             modifier = Modifier
-                                .padding(16.dp)
-                                .fillMaxWidth()
+                                .padding(top = 10.dp)
+                                .width(352.dp)
+//                                .height(214.dp)
                         ) {
-                            Column(modifier = Modifier.padding(16.dp)) {
+                            Column(modifier = Modifier.padding(22.dp)) {
+                                // First Row: Circle Image + Column with name, age, gender, dob
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Box(
+                                        modifier = Modifier
+                                            .size(100.dp)
+                                            .clip(CircleShape)
+                                            .background(InputColor) // Placeholder color if no image
+                                    ) {
+//                                        Image(
+//                                            painter = painterResource(id = R.drawable.ic_avatar),
+//                                            contentDescription = "Avatar",
+//                                            modifier = Modifier.fillMaxSize()
+//                                        )
+                                    }
+                                    Spacer(modifier = Modifier.width(26.dp))
+                                    Column {
+                                        Text(
+                                            text = pet.name, // Name
+                                            style = TextStyle(
+                                                fontWeight = FontWeight.Bold,
+                                                fontSize = 24.sp )
+                                        )
+                                        Text(
+                                            text = "${pet.age} years old", // Age
+                                            style = TextStyle(
+                                                fontWeight = FontWeight.Bold,
+                                                fontSize = 16.sp)
+                                        )
+                                        Text(
+                                            text = "${pet.gender}", // Gender
+                                            style = TextStyle(
+                                                fontWeight = FontWeight.Bold,
+                                                fontSize = 16.sp)                                        )
+                                        Text(
+                                            text = "${pet.dateOfBirth}", // DOB
+                                            style = TextStyle(
+                                                fontWeight = FontWeight.Bold,
+                                                fontSize = 16.sp)                                        )
+                                    }
+                                }
+                                Spacer(modifier = Modifier.height(20.dp))
+
+                                // Second Row: Medical Condition
                                 Text(
-                                    text = "${pet.name} - ${pet.breed}",
-                                    style = TextStyle(fontWeight = FontWeight.Bold)
+                                    text = "*Medical Condition: ${pet.medicalCondition}",
+                                    style = TextStyle(
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 16.sp
+                                    ),
                                 )
-                                Text(
-                                    text = "Age: ${pet.age} years",
-                                    style = TextStyle(fontWeight = FontWeight.Normal)
-                                )
+                                Spacer(modifier = Modifier.height(8.dp))
                             }
                         }
                     }
-                } else {
+            } else {
                     // Show "No pets available" if the user has no pets
                     Card(
-                        shape = RoundedCornerShape(16.dp),
-                        colors = CardDefaults.cardColors(containerColor = Color(0xFFF8F8F8)),
+                        shape = RoundedCornerShape(15.dp),
+                        colors = CardDefaults.cardColors(containerColor = Color.White),
+                        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
                         modifier = Modifier
-                            .padding(16.dp)
-                            .fillMaxWidth()
-                    ) {
-                        Column(modifier = Modifier.padding(16.dp)) {
+                            .padding(top = 150.dp)
+                            .width(352.dp)
+                                .height(214.dp),
+                        onClick = {
+                            navController.navigate("addNewPet") // Navigate to AddNewPetScreen
+                        }
+                    ){
+                        Column(
+                            modifier = Modifier.fillMaxSize()
+                                                .padding(16.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+
+                            ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.baseline_add_circle_outline_26),
+                                contentDescription = "Add New Pet"
+                            )
+
+                            Spacer(modifier = Modifier.height(8.dp))
+
                             Text(
-                                text = "Add your first pet to start",
+                                text = "Add your first pet to start !",
                                 style = TextStyle(fontWeight = FontWeight.Bold)
                             )
-                            Spacer(modifier = Modifier.height(8.dp))
-                            Button(onClick = {
-                                navController.navigate("addNewPet") // Navigate to AddNewPetScreen
-                            }) {
-                                Text("Add Pet")
-                            }
+
+
                         }
                     }
                 }
